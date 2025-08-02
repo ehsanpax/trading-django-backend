8f565a258c1862fe4e55f50cf885480a63dde3ec
mahsanps <mahsan.ps@gmail.com>
2025-08-02 13:48:52 +0330

    modify serializer

--- fundamental/serializers.py
+++ fundamental/serializers.py
@@ -3,11 +3,6 @@ from rest_framework import serializers
 from .models import EconomicCalendar
 
 class EconomicCalendarSerializer(serializers.ModelSerializer):
-    session_id = serializers.CharField(write_only=True, required=False)
-    execution_id = serializers.CharField(write_only=True, required=False)
-    date_from = serializers.DateField(write_only=True, required=False)
-    date_to = serializers.DateField(write_only=True, required=False)
-    currency = serializers.DateField(write_only=True, required=False)
 
     class Meta:
         model = EconomicCalendar
