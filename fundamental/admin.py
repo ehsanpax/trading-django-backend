6f7783d1df42434c9823e1dd5efef54aed76f3c8
mahsanps <mahsan.ps@gmail.com>
2025-08-02 09:37:24 +0330

    register-admin

--- fundamental/admin.py
+++ fundamental/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import EconomicCalendar
 
-# Register your models here.
+@admin.register(EconomicCalendar)
+class EconomicCalendarAdmin(admin.ModelAdmin):
+    list_display = ('event', 'event_time', 'country', 'impact', 'actual', 'previous', 'forecast', 'currency')
+    list_filter = ('country', 'impact', 'currency')
+    search_fields = ('event', 'country', 'currency__name')  
