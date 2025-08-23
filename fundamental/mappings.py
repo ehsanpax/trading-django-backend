267e17e6c96152ce52bb8d1b99f36823391d47ef
mahsanps <mahsan.ps@gmail.com>
2025-08-23 12:51:40 +0330

    COT report

---
+++ fundamental/mappings.py
@@ -0,0 +1,186 @@
+
+
+MAPPING = {
+    # =========================
+    # Meta Information
+    # =========================
+    "Market_and_Exchange_Names": "market_and_exchange_names",
+    "As_of_Date_In_Form_YYMMDD": "as_of_date_yyymmdd",
+    "Report_Date_as_YYYY-MM-DD": "as_of_date",
+    "CFTC_Contract_Market_Code": "cftc_contract_market_code",
+    "CFTC_Market_Code": "cftc_market_code_initials",
+    "CFTC_Region_Code": "cftc_region_code",
+    "CFTC_Commodity_Code": "cftc_commodity_code",
+
+    # =========================
+    # Open Interest
+    # =========================
+    "Open_Interest_All": "open_interest_all",
+    "Open_Interest_Old": "open_interest_old",
+    "Open_Interest_Other": "open_interest_other",
+
+    # =========================
+    # Noncommercial Positions
+    # =========================
+    "NonComm_Long_All": "noncomm_long_all",
+    "NonComm_Short_All": "noncomm_short_all",
+    "NonComm_Spread_All": "noncomm_spread_all",
+
+    "NonComm_Long_Old": "noncomm_long_old",
+    "NonComm_Short_Old": "noncomm_short_old",
+    "NonComm_Spread_Old": "noncomm_spread_old",
+
+    "NonComm_Long_Other": "noncomm_long_other",
+    "NonComm_Short_Other": "noncomm_short_other",
+    "NonComm_Spread_Other": "noncomm_spread_other",
+
+    # =========================
+    # Commercial Positions
+    # =========================
+    "Comm_Long_All": "comm_long_all",
+    "Comm_Short_All": "comm_short_all",
+    "Comm_Long_Old": "comm_long_old",
+    "Comm_Short_Old": "comm_short_old",
+    "Comm_Long_Other": "comm_long_other",
+    "Comm_Short_Other": "comm_short_other",
+
+    # =========================
+    # Total Reportable Positions
+    # =========================
+    "TotRept_Long_All": "total_reportable_long_all",
+    "TotRept_Short_All": "total_reportable_short_all",
+    "TotRept_Long_Old": "total_reportable_long_old",
+    "TotRept_Short_Old": "total_reportable_short_old",
+    "TotRept_Long_Other": "total_reportable_long_other",
+    "TotRept_Short_Other": "total_reportable_short_other",
+
+    # =========================
+    # Nonreportable Positions
+    # =========================
+    "NonRept_Long_All": "nonreportable_long_all",
+    "NonRept_Short_All": "nonreportable_short_all",
+    "NonRept_Long_Old": "nonreportable_long_old",
+    "NonRept_Short_Old": "nonreportable_short_old",
+    "NonRept_Long_Other": "nonreportable_long_other",
+    "NonRept_Short_Other": "nonreportable_short_other",
+
+    # =========================
+    # Changes
+    # =========================
+    "Change_Open_Interest_All": "change_open_interest_all",
+    "Change_NonComm_Long_All": "change_noncomm_long_all",
+    "Change_NonComm_Short_All": "change_noncomm_short_all",
+    "Change_NonComm_Spread_All": "change_noncomm_spread_all",
+    "Change_Comm_Long_All": "change_comm_long_all",
+    "Change_Comm_Short_All": "change_comm_short_all",
+    "Change_TotRept_Long_All": "change_total_reportable_long_all",
+    "Change_TotRept_Short_All": "change_total_reportable_short_all",
+    "Change_NonRept_Long_All": "change_nonreportable_long_all",
+    "Change_NonRept_Short_All": "change_nonreportable_short_all",
+
+    # =========================
+    # Percentages (All / Old / Other)
+    # =========================
+    "Pct_OI_All": "pct_oi_all",
+    "Pct_NonComm_Long_All": "pct_noncomm_long_all",
+    "Pct_NonComm_Short_All": "pct_noncomm_short_all",
+    "Pct_NonComm_Spread_All": "pct_noncomm_spread_all",
+    "Pct_Comm_Long_All": "pct_comm_long_all",
+    "Pct_Comm_Short_All": "pct_comm_short_all",
+    "Pct_TotRept_Long_All": "pct_total_reportable_long_all",
+    "Pct_TotRept_Short_All": "pct_total_reportable_short_all",
+    "Pct_NonRept_Long_All": "pct_nonreportable_long_all",
+    "Pct_NonRept_Short_All": "pct_nonreportable_short_all",
+
+    "Pct_OI_Old": "pct_oi_old",
+    "Pct_NonComm_Long_Old": "pct_noncomm_long_old",
+    "Pct_NonComm_Short_Old": "pct_noncomm_short_old",
+    "Pct_NonComm_Spread_Old": "pct_noncomm_spread_old",
+    "Pct_Comm_Long_Old": "pct_comm_long_old",
+    "Pct_Comm_Short_Old": "pct_comm_short_old",
+    "Pct_TotRept_Long_Old": "pct_total_reportable_long_old",
+    "Pct_TotRept_Short_Old": "pct_total_reportable_short_old",
+    "Pct_NonRept_Long_Old": "pct_nonreportable_long_old",
+    "Pct_NonRept_Short_Old": "pct_nonreportable_short_old",
+
+    "Pct_OI_Other": "pct_oi_other",
+    "Pct_NonComm_Long_Other": "pct_noncomm_long_other",
+    "Pct_NonComm_Short_Other": "pct_noncomm_short_other",
+    "Pct_NonComm_Spread_Other": "pct_noncomm_spread_other",
+    "Pct_Comm_Long_Other": "pct_comm_long_other",
+    "Pct_Comm_Short_Other": "pct_comm_short_other",
+    "Pct_TotRept_Long_Other": "pct_total_reportable_long_other",
+    "Pct_TotRept_Short_Other": "pct_total_reportable_short_other",
+    "Pct_NonRept_Long_Other": "pct_nonreportable_long_other",
+    "Pct_NonRept_Short_Other": "pct_nonreportable_short_other",
+
+    # =========================
+    # Traders (All / Old / Other)
+    # =========================
+    "Traders_Tot_All": "traders_total_all",
+    "Traders_NonComm_Long_All": "traders_noncomm_long_all",
+    "Traders_NonComm_Short_All": "traders_noncomm_short_all",
+    "Traders_NonComm_Spread_All": "traders_noncomm_spread_all",
+    "Traders_Comm_Long_All": "traders_comm_long_all",
+    "Traders_Comm_Short_All": "traders_comm_short_all",
+    "Traders_TotRept_Long_All": "traders_total_reportable_long_all",
+    "Traders_TotRept_Short_All": "traders_total_reportable_short_all",
+
+    "Traders_Tot_Old": "traders_total_old",
+    "Traders_NonComm_Long_Old": "traders_noncomm_long_old",
+    "Traders_NonComm_Short_Old": "traders_noncomm_short_old",
+    "Traders_NonComm_Spread_Old": "traders_noncomm_spread_old",
+    "Traders_Comm_Long_Old": "traders_comm_long_old",
+    "Traders_Comm_Short_Old": "traders_comm_short_old",
+    "Traders_TotRept_Long_Old": "traders_total_reportable_long_old",
+    "Traders_TotRept_Short_Old": "traders_total_reportable_short_old",
+
+    "Traders_Tot_Other": "traders_total_other",
+    "Traders_NonComm_Long_Other": "traders_noncomm_long_other",
+    "Traders_NonComm_Short_Other": "traders_noncomm_short_other",
+    "Traders_NonComm_Spread_Other": "traders_noncomm_spread_other",
+    "Traders_Comm_Long_Other": "traders_comm_long_other",
+    "Traders_Comm_Short_Other": "traders_comm_short_other",
+    "Traders_TotRept_Long_Other": "traders_total_reportable_long_other",
+    "Traders_TotRept_Short_Other": "traders_total_reportable_short_other",
+
+    # =========================
+    # Concentration Ratios (All / Old / Other)
+    # =========================
+    "Conc_Gross_4_Long_All": "conc_gross_4_long_all",
+    "Conc_Gross_4_Short_All": "conc_gross_4_short_all",
+    "Conc_Gross_8_Long_All": "conc_gross_8_long_all",
+    "Conc_Gross_8_Short_All": "conc_gross_8_short_all",
+    "Conc_Net_4_Long_All": "conc_net_4_long_all",
+    "Conc_Net_4_Short_All": "conc_net_4_short_all",
+    "Conc_Net_8_Long_All": "conc_net_8_long_all",
+    "Conc_Net_8_Short_All": "conc_net_8_short_all",
+
+    "Conc_Gross_4_Long_Old": "conc_gross_4_long_old",
+    "Conc_Gross_4_Short_Old": "conc_gross_4_short_old",
+    "Conc_Gross_8_Long_Old": "conc_gross_8_long_old",
+    "Conc_Gross_8_Short_Old": "conc_gross_8_short_old",
+    "Conc_Net_4_Long_Old": "conc_net_4_long_old",
+    "Conc_Net_4_Short_Old": "conc_net_4_short_old",
+    "Conc_Net_8_Long_Old": "conc_net_8_long_old",
+    "Conc_Net_8_Short_Old": "conc_net_8_short_old",
+
+    "Conc_Gross_4_Long_Other": "conc_gross_4_long_other",
+    "Conc_Gross_4_Short_Other": "conc_gross_4_short_other",
+    "Conc_Gross_8_Long_Other": "conc_gross_8_long_other",
+    "Conc_Gross_8_Short_Other": "conc_gross_8_short_other",
+    "Conc_Net_4_Long_Other": "conc_net_4_long_other",
+    "Conc_Net_4_Short_Other": "conc_net_4_short_other",
+    "Conc_Net_8_Long_Other": "conc_net_8_long_other",
+    "Conc_Net_8_Short_Other": "conc_net_8_short_other",
+
+    # =========================
+    # Contract & Report Info
+    # =========================
+    "Contract_Units": "contract_units",
+    "CFTC_Contract_Market_Code_Quotes": "cftc_contract_market_code_quotes",
+    "CFTC_Market_Code_Quotes": "cftc_market_code_initials_quotes",
+    "CFTC_Commodity_Code_Quotes": "cftc_commodity_code_quotes",
+    "FutOnly_or_Combined": "report_type",
+    "COT_Date": "cot_date",
+}
--- fundamental/urls.py
+++ fundamental/urls.py
@@ -1,6 +1,6 @@
 # fundumental/urls.py
 from django.urls import path
-from fundamental.views import EconomicCalendarAPIView, EconomicCalendarEventListAPIView, NewsAPIView, NewsListAPIView
+from fundamental.views import EconomicCalendarAPIView, EconomicCalendarEventListAPIView, NewsAPIView, NewsListAPIView, COTReportListAPIView, COTReportAPIView
 
 urlpatterns = [
     path(
@@ -15,6 +15,13 @@ urlpatterns = [
         name="news_api",
     ),
     path('news-list/', NewsListAPIView.as_view(), name='news-list'),
+    path(
+        "cot-api/",
+        COTReportAPIView.as_view(),
+        name="cot_api",
+    ),
+    path('cot-list/', COTReportListAPIView.as_view(), name='cot-list'),
+
 
 ]
 
