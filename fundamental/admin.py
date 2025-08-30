ecf3436c34d8b3d4d2d0a9b16171004905808a2d
mahsanps <mahsan.ps@gmail.com>
2025-08-30 10:46:36 +0330

    cot django admin

--- fundamental/admin.py
+++ fundamental/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import EconomicCalendar, News
+from .models import EconomicCalendar, News, COTReport
 
 @admin.register(EconomicCalendar)
 class EconomicCalendarAdmin(admin.ModelAdmin):
@@ -13,3 +13,20 @@ class NewsAdmin(admin.ModelAdmin):
     list_filter = ('source', 'time')
     search_fields = ('headline', 'source', 'content')
     ordering = ('-time',)
+
+
+@admin.register(COTReport)
+class COTReportAdmin(admin.ModelAdmin):
+    list_display = (
+        "market_and_exchange_names",
+        "as_of_date",
+        "cot_date",
+        "report_type",
+        "open_interest_all",
+        "noncomm_long_all",
+        "comm_long_all",
+        "comm_short_all",
+    )
+    list_filter = ("report_type", "as_of_date", "cot_date")
+    search_fields = ("market_and_exchange_names", "cftc_contract_market_code", "cftc_commodity_code")
+    ordering = ("-as_of_date",)
